612f5d04c250dc18c189a961df3a218150e2fa96
呼吸二氧化碳 <jonwang@queyang.com>
2013-09-04 18:26:15 +0800

    完善安装包

--- src/MyQEE/Installer/Installer.php
+++ src/MyQEE/Installer/Installer.php
@@ -1,13 +1,74 @@
 <?php
 namespace MyQEE\Installer;
 
-use Composer\Installers\BaseInstaller;
 
-class Installer extends BaseInstaller
+use Composer\Package\PackageInterface;
+use Composer\Installer\LibraryInstaller;
+
+class Installer extends LibraryInstaller
 {
     protected $locations = array
     (
-        'core'    => 'core/',
-        'library' => 'libraries/{$name}/',
+        'myqee-core'    => 'core/',
+        'myqee-library' => 'libraries/{$vendor}/{$name}/',
     );
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getInstallPath(PackageInterface $package)
+    {
+        $packageType = $this->package->getType();
+
+        if (!isset($this->locations[$packageType]))
+        {
+            throw new \InvalidArgumentException(sprintf('Package type "%s" is not supported', $packageType));
+        }
+
+        $availableVars = $this->inflectPackageVars(compact('name', 'vendor', 'type'));
+
+        $extra = $package->getExtra();
+        if (!empty($extra['installer-name']))
+        {
+            $availableVars['name'] = $extra['installer-name'];
+        }
+
+        $path = $this->locations[$packageType];
+        foreach (array('name', 'vendor', 'type') as $item)
+        {
+            $path = str_replace('{$'.$item.'}', $availableVars[$item], $page);
+        }
+
+        return $path;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function supports($packageType)
+    {
+        return isset($this->locations[$packageType]);
+    }
+
+    /**
+     * For an installer to override to modify the vars per installer.
+     *
+     * @param  array $vars
+     * @return array
+     */
+    public function inflectPackageVars($vars)
+    {
+        return $vars;
+    }
+
+    /**
+     * For an installer to override to modify the vars per installer.
+     *
+     * @param  array $vars
+     * @return array
+     */
+    public function inflectPackageVars($vars)
+    {
+        return $vars;
+    }
 }
